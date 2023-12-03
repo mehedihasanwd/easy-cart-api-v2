998793bb9b03705bfe5ca1a9c71a3a40e05e2bf8
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-03 22:06:23 +0600

    Add staff controllers, services type

---
+++ src/types/staff/staff_controllers_services_type.ts
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+import { IStaffDocument } from "./staff_document_type";
+import * as common_type from "../common_type";
+
+// controllers type
+export interface IRegisterStaff extends common_type.IRegister {}
+
+export interface ILoginStaff extends common_type.ILogin {}
+
+export interface IUpdateStaff
+  extends common_type.IName,
+    common_type.IGender,
+    common_type.IPhone,
+    common_type.ICountry,
+    common_type.ICity,
+    common_type.IHouseNumberOrName,
+    common_type.IPostCode {}
+
+export interface IUpdateStaffRole extends common_type.IStaffRole {}
+
+export interface IForgotStaffPassword extends common_type.IForgotPassword {}
+
+export interface IResetStaffPassword extends common_type.IResetPassword {}
+
+export interface IChangeStaffPassword extends common_type.IChangePassword {}
+
+// services type
+interface IStaff {
+  staff: IStaffDocument;
+}
+
+export interface IFindStaffByPropServiceParam {
+  key: "_id" | "email";
+  value: string | mongoose.Types.ObjectId;
+}
+
+export interface IFindStaffsServiceParam
+  extends common_type.ISkipLimitOptional {}
+
+export interface ICreateNewStaffServiceParam {
+  data: IRegisterStaff;
+}
+
+export interface IUpdateStaffByIdServiceParam extends common_type.IDocumentId {
+  data: IUpdateStaff;
+}
+
+export interface IUpdateStaffRoleByIdServiceParam
+  extends common_type.IDocumentId {
+  data: IUpdateStaffRole;
+}
+
+export interface IMatchStaffPasswordServiceParam
+  extends common_type.IPassword,
+    IStaff {}
+
+export interface IUpdateStaffPasswordServiceParam
+  extends common_type.INewPassword,
+    IStaff {}
+
+export interface IUpdateStaffImageByIdServiceParam
+  extends common_type.IDocumentId,
+    common_type.IImage {}
+
+export interface ICreateStaffAuthTokensServiceParam extends IStaff {}
