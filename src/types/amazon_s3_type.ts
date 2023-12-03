5cdbbbfa7da14ad8055190dead5e743f42956c6a
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-03 07:12:28 +0600

    Add types for amazon s3 utility functions

---
+++ src/types/amazon_s3_type.ts
@@ -0,0 +1,31 @@
+import { UploadedFile } from "express-fileupload";
+import { S3 } from "aws-sdk";
+
+export interface IS3UploadParams {
+  Bucket: string;
+  Key: string;
+  Body: Buffer;
+  ContentType: string;
+  MetaData: {
+    "Content-Disposition": string;
+  };
+}
+
+export interface IRemoveImageFromS3Params {
+  Bucket: string;
+  Key: string;
+}
+
+export interface IUploadImageToS3Params {
+  image: UploadedFile;
+}
+
+export interface IUploadedImageKey {
+  image_key: string;
+}
+
+export type TS3ManagedUpload = S3.ManagedUpload.SendData;
+
+export interface ICloudfrontImageUrl {
+  cloud_image: TS3ManagedUpload;
+}
