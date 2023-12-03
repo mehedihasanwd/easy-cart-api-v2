40bc0466d3b93d6b6c47b9dd8cd56f21910c01b1
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-03 07:00:58 +0600

    Create app and configure routes, app middleware, globalErrorHandler

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,26 @@
 import express from "express";
+import { IError } from "../types/error_type";
+import * as responses from "../utils/response";
+import middleware from "./middleware";
+import routes from "../routes";
 
 const app: express.Application = express();
+
+const globalErrorHandler: express.ErrorRequestHandler = (
+  err: IError,
+  req,
+  res,
+  next
+) => {
+  const message: string = err.message || "Something went wrong!";
+  const status: number = err.status || 500;
+  return responses.responseErrorMessage(res, status, {
+    error: message,
+  });
+};
+
+app.use(middleware);
+app.use(routes);
+app.use(globalErrorHandler);
 
 export default app;
