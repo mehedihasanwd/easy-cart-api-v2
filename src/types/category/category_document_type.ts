1af701bf9df6d31d3c54457be84748f8f09dcba9
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-03 15:25:07 +0600

    Add category HydratedDocument type

---
+++ src/types/category/category_document_type.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import { ICategorySchema } from "./category_model_type";
+
+export interface ICategoryDocument extends mongoose.Document, ICategorySchema {
+  _id: mongoose.Types.ObjectId;
+}
+
+export type THydratedCategoryDocument =
+  mongoose.HydratedDocument<ICategoryDocument>;
