ad3cb148f5e0b5106e9d7ad40ab700eab19d78bd
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-03 15:26:27 +0600

    Add category controllers, services type

---
+++ src/types/category/category_controller_services_type.ts
@@ -0,0 +1,39 @@
+import * as common_type from "../common_type";
+
+// controllers type
+export interface IPostNewCategory
+  extends common_type.ICategoryName,
+    common_type.IDescription {}
+
+export interface ICategoryStatus extends common_type.IStatus {}
+
+// services type
+export interface IFindCategoryByPropServiceParam extends common_type.IValue {
+  key: "_id" | "name" | "status";
+}
+
+export interface ICreateNewCategoryServiceData {
+  data: {
+    name: string;
+    description: string;
+    slug: string;
+    image: common_type.IImageProps;
+  };
+}
+
+export interface IUpdateCategoryImageByIdServiceParam
+  extends common_type.IDocumentId,
+    common_type.IImage {}
+
+export interface IUpdateCategoryStatusByIdServiceParam
+  extends common_type.IDocumentId,
+    common_type.IStatus {}
+
+export interface IUpdateCategoryByIdServiceData
+  extends common_type.IDocumentId {
+  data: {
+    name: string;
+    description: string;
+    slug: string;
+  };
+}
