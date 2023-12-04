c771774cce55c2fc81b25a42c15bb2b29076f3dd
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-04 21:47:16 +0600

    Configure server port (server.ts)

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,14 @@ import http from "http";
 import dotenvconfig from "./config/dotenvconfig";
 import app from "./app";
 
+const port: number = dotenvconfig.PORT;
 const server: http.Server = http.createServer(app);
 
 connnectDatabase(dotenvconfig.MONGO_URI)
   .then(() => {
     console.log("Database connected");
-    server.listen(() => {
-      console.log(`Server is listening on port: ${dotenvconfig.PORT}`);
+    server.listen(port, () => {
+      console.log(`Server is listening on port: ${port}`);
     });
   })
   .catch((e: mongoose.MongooseError) => {
